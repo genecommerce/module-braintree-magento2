41d7700c27cdbd1050ff289c2f8f755a63757d02
Aidan Threadgold <aidan@gene.co.uk>
2018-06-20 16:52:56 +0100

    Patch for Apple Pay US Tax

--- view/frontend/web/js/applepay/api.js
+++ view/frontend/web/js/applepay/api.js
@@ -13,7 +13,45 @@ define(['uiComponent', 'mage/translate', 'mage/storage', 'jquery'], function (Co
             actionSuccess: null,
             grandTotalAmount: 0,
             isLoggedIn: false,
-            storeCode: "default"
+            storeCode: "default",
+            shippingAddress: {},
+            countryDirectory: null
+        },
+
+        initialize: function () {
+            this._super();
+                if (!this.countryDirectory) {
+                    storage.get("rest/V1/directory/countries").done(function(result) {
+                    this.countryDirectory = {};
+                    var i, data, x, region;
+                    for (i=0; i < result.length; ++i) {
+                        data = result[i];
+                        this.countryDirectory[ data.two_letter_abbreviation ] = {};
+                        for (x=0; x < data.available_regions.length; ++x) {
+                            region = data.available_regions[x];
+                            this.countryDirectory[ data.two_letter_abbreviation ][ region.name.toLowerCase().replace(/[^A-Z0-9]/ig, '') ] = region.id;
+                        }
+                    }
+                }.bind(this));
+            }
+        },
+
+        /**
+         * Get region ID
+         */
+        getRegionId: function(countryCode, regionName) {
+            if (typeof regionName !== 'string') {
+                return null;
+            }
+
+            regionName = regionName.toLowerCase().replace(/[^A-Z0-9]/ig, '');
+            if (typeof this.countryDirectory[countryCode] !== 'undefined') {
+                if (typeof this.countryDirectory[countryCode][regionName] !== 'undefined') {
+                    return this.countryDirectory[countryCode][regionName];
+                }
+            }
+
+            return 0;
         },
 
         /**
@@ -121,62 +159,84 @@ define(['uiComponent', 'mage/translate', 'mage/storage', 'jquery'], function (Co
                     address: {
                         city: address.locality,
                         region: address.administrativeArea,
-                        country_id: address.countryCode,
+                        country_id: address.countryCode.toUpperCase(),
                         postcode: address.postalCode,
                         save_in_address_book :0
                     }
                 };
 
-            // Retrieve shipping methods
-            storage.post(
-                this.getApiUrl("estimate-shipping-methods"),
-                JSON.stringify(payload)
-            ).done(function (r) {
-                var shippingMethods = [],
-                    method = {};
-                this.shippingMethods = {};
+            this.shippingAddress = payload.address;
 
-                // Stop if no shipping methods
-                if (r.length == 0) {
-                    session.abort();
-                    alert($t("There are no shiping methods available for you right now - please try an again or use an alternative payment method."));
-                    return false;
-                }
-
-                // Format shipping methods array
-                for (var i = 0; i < r.length; i++) {
-                    method = {
-                        identifier: r[i].method_code,
-                        label: r[i].method_title,
-                        detail: r[i].carrier_title ? r[i].carrier_title : "",
-                        amount: parseFloat(r[i].amount).toFixed(2)
-                    };
-                    shippingMethods.push(method);
-                    this.shippingMethods[ r[i].method_code ] = r[i];
-                }
-
-                // Set the shipping method to the first option
-                this.shippingMethod = r[0].method_code;
-
-                // Pass shipping methods back
-                session.completeShippingContactSelection(
-                    ApplePaySession.STATUS_SUCCESS,
-                    shippingMethods,
-                    {
-                        label: this.getDisplayName(),
-                        amount: parseFloat(this.getGrandTotalAmount() + parseFloat(shippingMethods[0].amount)).toFixed(2)
+            var totalsPayload = {
+                "addressInformation": {
+                    "address": {
+                        "countryId": this.shippingAddress.country_id,
+                        "region": this.shippingAddress.region,
+                        "regionId": this.getRegionId(this.shippingAddress.country_id, this.shippingAddress.region),
+                        "postcode": this.shippingAddress.postcode
                     },
-                    [{
-                        type: 'final',
-                        label: $t('Shipping'),
-                        amount: shippingMethods[0].amount
-                    }]
-                );
-            }.bind(this)).fail(function (r) {
-                session.abort();
-                console.error("Braintree ApplePay Unable to find shipping methods for estimate-shipping-methods", r);
-                alert($t("We were unable to find any shipping methods for you. Please try an alternative payment method."));
-            });
+                    "shipping_method_code": "",
+                    "shipping_carrier_code": ""
+                }
+            };
+
+            storage.post(
+                this.getApiUrl("totals-information"),
+                JSON.stringify(totalsPayload)
+            ).done(function (r) {
+                this.setGrandTotalAmount(r.base_grand_total);
+
+                // Retrieve shipping methods
+                storage.post(
+                    this.getApiUrl("estimate-shipping-methods"),
+                    JSON.stringify(payload)
+                ).done(function (r) {
+                    var shippingMethods = [],
+                        method = {};
+                    this.shippingMethods = {};
+
+                    // Stop if no shipping methods
+                    if (r.length == 0) {
+                        session.abort();
+                        alert($t("There are no shiping methods available for you right now - please try an again or use an alternative payment method."));
+                        return false;
+                    }
+
+                    // Format shipping methods array
+                    for (var i = 0; i < r.length; i++) {
+                        method = {
+                            identifier: r[i].method_code,
+                            label: r[i].method_title,
+                            detail: r[i].carrier_title ? r[i].carrier_title : "",
+                            amount: parseFloat(r[i].amount).toFixed(2)
+                        };
+                        shippingMethods.push(method);
+                        this.shippingMethods[ r[i].method_code ] = r[i];
+                    }
+
+                    // Set the shipping method to the first option
+                    this.shippingMethod = r[0].method_code;
+
+                    // Pass shipping methods back
+                    session.completeShippingContactSelection(
+                        ApplePaySession.STATUS_SUCCESS,
+                        shippingMethods,
+                        {
+                            label: this.getDisplayName(),
+                            amount: parseFloat(this.getGrandTotalAmount() + parseFloat(shippingMethods[0].amount)).toFixed(2)
+                        },
+                        [{
+                            type: 'final',
+                            label: $t('Shipping'),
+                            amount: shippingMethods[0].amount
+                        }]
+                    );
+                }.bind(this)).fail(function (r) {
+                    session.abort();
+                    console.error("Braintree ApplePay Unable to find shipping methods for estimate-shipping-methods", r);
+                    alert($t("We were unable to find any shipping methods for you. Please try an alternative payment method."));
+                });
+            }.bind(this));
         },
 
         /**
@@ -186,18 +246,38 @@ define(['uiComponent', 'mage/translate', 'mage/storage', 'jquery'], function (Co
             var shippingMethod = event.shippingMethod;
             this.shippingMethod = shippingMethod.identifier;
 
-            session.completeShippingMethodSelection(
-                ApplePaySession.STATUS_SUCCESS,
-                {
-                    label: this.getDisplayName(),
-                    amount: this.getGrandTotalAmount() + parseFloat(shippingMethod.amount)
-                },
-                [{
-                    type: 'final',
-                    label: $t('Shipping'),
-                    amount: shippingMethod.amount
-                }]
-            );
+            var payload = {
+                "addressInformation": {
+                    "address": {
+                        "countryId": this.shippingAddress.country_id,
+                        "region": this.shippingAddress.region,
+                        "regionId": this.getRegionId(this.shippingAddress.country_id, this.shippingAddress.region),
+                        "postcode": this.shippingAddress.postcode
+                    },
+                    "shipping_method_code": this.shippingMethods[ this.shippingMethod ].method_code,
+                    "shipping_carrier_code": this.shippingMethods[ this.shippingMethod ].carrier_code
+                }
+            };
+
+            storage.post(
+                this.getApiUrl("totals-information"),
+                JSON.stringify(payload)
+            ).done(function (r) {
+                this.setGrandTotalAmount(r.base_grand_total);
+
+                session.completeShippingMethodSelection(
+                    ApplePaySession.STATUS_SUCCESS,
+                    {
+                        label: this.getDisplayName(),
+                        amount: this.getGrandTotalAmount() + parseFloat(shippingMethod.amount)
+                    },
+                    [{
+                        type: 'final',
+                        label: $t('Shipping'),
+                        amount: shippingMethod.amount
+                    }]
+                );
+            }.bind(this));
         },
 
         /**
@@ -216,9 +296,9 @@ define(['uiComponent', 'mage/translate', 'mage/storage', 'jquery'], function (Co
                             "street": shippingContact.addressLines,
                             "city": shippingContact.locality,
                             "region": shippingContact.administrativeArea,
-                            "region_id": null,
+                            "region_id": this.getRegionId(shippingContact.countryCode.toUpperCase(), shippingContact.administrativeArea),
                             "region_code": null,
-                            "country_id": shippingContact.countryCode,
+                            "country_id": shippingContact.countryCode.toUpperCase(),
                             "postcode": shippingContact.postalCode,
                             "same_as_billing": 0,
                             "customer_address_id": 0,
@@ -232,9 +312,9 @@ define(['uiComponent', 'mage/translate', 'mage/storage', 'jquery'], function (Co
                             "street": billingContact.addressLines,
                             "city": billingContact.locality,
                             "region": billingContact.administrativeArea,
-                            "region_id": null,
+                            "region_id": this.getRegionId(billingContact.countryCode.toUpperCase(), billingContact.administrativeArea),
                             "region_code": null,
-                            "country_id": billingContact.countryCode,
+                            "country_id": billingContact.countryCode.toUpperCase(),
                             "postcode": billingContact.postalCode,
                             "same_as_billing": 0,
                             "customer_address_id": 0,
