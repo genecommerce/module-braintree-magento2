8956627daa9a9b3df1670395de1f6dab12b27935
Paul Canning <paul.canning@gene.co.uk>
2019-08-21 10:45:55 +0100

    Merchant and Property ID now set via Block class.

--- Block/Paypal/Shopping.php
+++ Block/Paypal/Shopping.php
@@ -14,10 +14,35 @@ use Magento\Framework\View\Element\Template;
 class Shopping extends Template
 {
     /**
+     * Client ID from PayPal account.
+     * Passed as a GET param when loading the PayPal JS SDK.
+     *
      * @return string
      */
     public function getClientId(): string
     {
         return 'AYkryi9mte23ZuKo62tAXnyDikAYXCJcnMDcvOqspH8KmhORaSP6tqu3eCNWE3-twi1YIFXV-4Xwoibb';
+    }
+
+    /**
+     * Passed as a GET param when loading the PayPal JS SDK.
+     * This will be set in the config values, after the onboarding process.
+     *
+     * @return string
+     */
+    public function getMerchantId(): string
+    {
+        return '8YC9P5KG8RGUQ';
+    }
+
+    /**
+     * Property ID used when setting up the PayPal Tracker object.
+     * This will be set in the config values, after the onboarding process.
+     *
+     * @return string
+     */
+    public function getPropertyId(): string
+    {
+        return 'cc2d8b95-39b0-430f-a9cf-4f6f5896f018';
     }
 }
--- view/frontend/web/js/paypal/shopping.js
+++ view/frontend/web/js/paypal/shopping.js
@@ -1,12 +1,31 @@
-define(['jquery'],function ($) {
+define(
+    [
+        'jquery'
+    ],
+    function (
+        $
+    ) {
         'use strict';
 
-        function initShopping(config) {
-            let tracker = paypal.Tracker({});
+        const encodeData = data => encodeURIComponent(btoa(JSON.stringify(data)));
+
+        const initTracker = config => {
+            return paypal.Tracker({
+                propertyId: config.propertyId,
+                paramsToBeaconUrl: params => {
+                    const { trackingType, data } = params;
+                    return `https://www.sandbox.paypal.com/targeting/track/${ trackingType }?data=${ encodeData(data) }`
+                },
+                paramsToTokenUrl: () => 'https://www.sandbox.paypal.com'
+            })
+        };
+
+        const initShopping = config => {
+            let tracker = initTracker(config);
+
             $(document).on('ajax:addToCart', function (event, data) {
-                console.log(data.form.data());
                 let productData = data.form.data();
-                tracker.addToCart({
+                tracker.setCart({
                     items: [{
                         id: productData.productSku,
                         title: 'Product Title',
@@ -18,8 +37,8 @@ define(['jquery'],function ($) {
                     total: "99.99"
                 });
             });
-        }
+        };
 
-        return initShopping;
+        return initShopping(config);
     }
 );
