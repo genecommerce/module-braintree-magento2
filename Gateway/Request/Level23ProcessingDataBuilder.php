e21672b6d3d43faf948b5ab3da05bce3b90349f6
Paul Canning <paul.canning@gene.co.uk>
2019-08-30 17:22:58 +0100

    Add Level 3 processing.

--- Gateway/Request/Level23ProcessingDataBuilder.php
+++ Gateway/Request/Level23ProcessingDataBuilder.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Magento\Braintree\Gateway\Request;
 
+use Braintree\TransactionLineItem;
+use League\ISO3166\ISO3166;
 use Magento\Braintree\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 
@@ -12,22 +14,44 @@ use Magento\Payment\Gateway\Request\BuilderInterface;
 class Level23ProcessingDataBuilder implements BuilderInterface
 {
     const KEY_PURCHASE_ORDER_NUMBER = 'purchaseOrderNumber';
-
-    const KEY_TAX_AMOUNT = 'taxAmount';
+    const KEY_TAX_AMT = 'taxAmount';
+    const KEY_SHIPPING_AMT = 'shippingAmount';
+    const KEY_DISCOUNT_AMT = 'discountAmount';
+    const KEY_SHIPPING = 'shipping';
+    const KEY_COUNTRY_CODE_ALPHA_3 = 'countryCodeAlpha3';
+    const KEY_LINE_ITEMS = 'lineItems';
+    const LINE_ITEMS_ARRAY = [
+        'name',
+        'kind',
+        'quantity',
+        'unitAmount',
+        'unitOfMeasure',
+        'totalAmount',
+        'taxAmount',
+        'discountAmount',
+        'productCode',
+        'commodityCode'
+    ];
 
     /**
      * @var SubjectReader
      */
     private $subjectReader;
+    /**
+     * @var ISO3166
+     */
+    private $iso3166;
 
     /**
      * Level23ProcessingDataBuilder constructor.
      *
      * @param SubjectReader $subjectReader
+     * @param ISO3166 $iso3166
      */
-    public function __construct(SubjectReader $subjectReader)
+    public function __construct(SubjectReader $subjectReader, ISO3166 $iso3166)
     {
         $this->subjectReader = $subjectReader;
+        $this->iso3166 = $iso3166;
     }
 
     /**
@@ -40,16 +64,39 @@ class Level23ProcessingDataBuilder implements BuilderInterface
     {
         $paymentDO = $this->subjectReader->readPayment($buildSubject);
         $order = $paymentDO->getOrder();
+        $billingAddress = $order->getBillingAddress();
+        $addressData = $this->iso3166->alpha2($billingAddress->getCountryId());
 
         $tax = 0;
+        $lineItems = [];
 
         foreach ($order->getItems() as $item) {
             $tax += $item->getTaxAmount();
+
+            $lineItems[] = array_combine(
+                self::LINE_ITEMS_ARRAY,
+                [
+                    $item->getName(),
+                    TransactionLineItem::DEBIT,
+                    $item->getQtyOrdered(),
+                    $item->getPrice(),
+                    $item->getProductType(),
+                    $item->getQtyOrdered() * $item->getPrice(),
+                    $item->getTaxAmount(),
+                    $item->getDiscountAmount(),
+                    $item->getSku(),
+                    $item->getSku()
+                ]
+            );
         }
 
         return [
             self::KEY_PURCHASE_ORDER_NUMBER => $order->getOrderIncrementId(),
-            self::KEY_TAX_AMOUNT => $tax
+            self::KEY_TAX_AMT => $tax,
+            self::KEY_LINE_ITEMS => $lineItems,
+            self::KEY_SHIPPING => [
+                self::KEY_COUNTRY_CODE_ALPHA_3 => $addressData['alpha3']
+            ]
         ];
     }
 }
