3b67330276d424712d71f57ee37c6c206e0e8e86
Paul Canning <paul.canning@gene.co.uk>
2019-09-03 10:46:14 +0100

    Add ACH JS SDK and update all SDKs to 3.48.0 as minimum requirement for ACH.

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -5,18 +5,19 @@
 var config = {
     map: {
         '*': {
-            braintree: 'https://js.braintreegateway.com/web/3.47.0/js/client.min.js',
+            braintree: 'https://js.braintreegateway.com/web/3.48.0/js/client.min.js',
         }
     },
 
     paths: {
-        "braintreePayPalCheckout": "https://js.braintreegateway.com/web/3.47.0/js/paypal-checkout.min",
-        "braintreeHostedFields": "https://js.braintreegateway.com/web/3.47.0/js/hosted-fields.min",
-        "braintreeDataCollector": "https://js.braintreegateway.com/web/3.47.0/js/data-collector.min",
-        "braintreeThreeDSecure": "https://js.braintreegateway.com/web/3.47.0/js/three-d-secure.min",
-        "braintreeApplePay": 'https://js.braintreegateway.com/web/3.47.0/js/apple-pay.min',
-        "braintreeGooglePay": 'https://js.braintreegateway.com/web/3.47.0/js/google-payment.min',
-        "braintreeVenmo": 'https://js.braintreegateway.com/web/3.47.0/js/venmo.min',
+        "braintreePayPalCheckout": "https://js.braintreegateway.com/web/3.48.0/js/paypal-checkout.min",
+        "braintreeHostedFields": "https://js.braintreegateway.com/web/3.48.0/js/hosted-fields.min",
+        "braintreeDataCollector": "https://js.braintreegateway.com/web/3.48.0/js/data-collector.min",
+        "braintreeThreeDSecure": "https://js.braintreegateway.com/web/3.48.0/js/three-d-secure.min",
+        "braintreeApplePay": 'https://js.braintreegateway.com/web/3.48.0/js/apple-pay.min',
+        "braintreeGooglePay": 'https://js.braintreegateway.com/web/3.48.0/js/google-payment.min',
+        "braintreeVenmo": 'https://js.braintreegateway.com/web/3.48.0/js/venmo.min',
+        "braintreeAch": "https://js.braintreegateway.com/web/3.48.0/js/us-bank-account.min.js",
         "googlePayLibrary": "https://pay.google.com/gp/p/js/pay"
     }
 };
