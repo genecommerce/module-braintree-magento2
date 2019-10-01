8ae383471c59fd4053a7904379b0ed323592606b
Paul Canning <paul.canning@gene.co.uk>
2019-10-01 17:11:58 +0100

    Improved fallback page JS.

--- Block/Lpm/Fallback.php
+++ Block/Lpm/Fallback.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Magento\Braintree\Block\Lpm;
 
+use Magento\Braintree\Model\Lpm\Config;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Template;
 
 /**
@@ -14,20 +17,47 @@ class Fallback extends Template
      * @var array $fallbackDataa
      */
     private $fallbackData;
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * Fallback constructor.
+     *
+     * @param Template\Context $context
+     * @param Config $config
+     * @param array $data
+     */
+    public function __construct(Template\Context $context, Config $config, array $data = [])
+    {
+        parent::__construct($context, $data);
+        $this->config = $config;
+    }
+
+    /**
+     * @return string
+     * @throws InputException
+     * @throws NoSuchEntityException
+     */
+    public function getClientToken(): string
+    {
+        return $this->config->getClientToken();
+    }
 
     /**
      * @return array
      */
-    public function getFallbackData()
+    public function getFallbackData(): array
     {
         return $this->fallbackData;
     }
 
     /**
      * @param array $data
-     * @return $this
+     * @return self
      */
-    public function setFallbackData(array $data)
+    public function setFallbackData(array $data): self
     {
         $this->fallbackData = $data;
         return $this;
--- view/frontend/web/js/lpm/fallback.js
+++ view/frontend/web/js/lpm/fallback.js
@@ -1,15 +1,84 @@
 define(
     [
-        'jquery'
+        'jquery',
+        'braintree',
+        'braintreeLpm',
+        'Magento_Ui/js/model/messageList',
+        'Magento_Ui/js/modal/alert',
+        'mage/translate'
     ], function (
-        $
+        $,
+        braintree,
+        lpm,
+        messageList,
+        alert,
+        $t
     ) {
         'use strict';
         $.widget('braintree.fallback', {
             _create: function () {
-                console.log(this.options.data)
+                var self = this;
+                $('body').trigger('processStart');
+
+                braintree.create({
+                    authorization: self.options.clientToken
+                }, function (clientError, clientInstance) {
+                    if (clientError) {
+                        $('body').trigger('processStop');
+                        self.createAlert(clientError.message);
+                        return;
+                    }
+
+                    lpm.create({
+                        client: clientInstance
+                    }, function (lpmError, lpmInstance) {
+                        if (lpmError) {
+                            $('body').trigger('processStop');
+                            self.createAlert(lpmError.message);
+                            return;
+                        }
+
+                        if (lpmInstance.hasTokenizationParams()) {
+                            lpmInstance.tokenize(function (tokenizeError, payload) {
+                                if (tokenizeError) {
+                                    // handle tokenization error
+                                    $('body').trigger('processStop');
+                                    self.createAlert(tokenizeError.message);
+                                    return;
+                                }
+
+                                // send payload.nonce to your server
+                                console.log(payload);
+
+                                $('body').trigger('processStop');
+                            });
+                        } else {
+                            // if this page should only be reached when
+                            // recovering from a mobile app switch,
+                            // display an error for not having the
+                            // correct params in the query string
+                            $('body').trigger('processStop');
+                        }
+                    });
+                });
+            },
+
+            createAlert: function (message, title = 'Whoops, something went wrong!', buttonText = 'Dismiss') {
+                alert({
+                    buttons: [{
+                        class: 'action-primary action-accept',
+                        click: function () {
+                            this.closeModal(true);
+                        },
+                        text: $t(buttonText)
+                    }],
+                    content: message,
+                    modalClass: 'alert',
+                    title: $t(title)
+                });
             }
         });
+
         return $.braintree.fallback;
     }
 );
