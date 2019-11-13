c624933da5c88b2e63fc9a7a26551f5036b47982
Paul Canning <paul.canning@gene.co.uk>
2019-11-13 17:19:11 +0000

    Remove unused Kount config options. Add functionality to let Admin skip fraud protection via config.

--- Model/Config/Source/KountEnsUrl.php
+++ Model/Config/Source/KountEnsUrl.php
@@ -3,19 +3,47 @@ declare(strict_types=1);
 
 namespace Magento\Braintree\Model\Config\Source;
 
+use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 
 /**
  * Class KountEnsUrl
+ *
+ * Displays Kount ENS endpoint URL in config.
  */
 class KountEnsUrl extends Field
 {
     /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * KountEnsUrl constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
      * {@inheritDoc}
+     * @param AbstractElement $element
+     * @return string
      */
     public function _getElementHtml(AbstractElement $element): string
     {
-        return 'foobar';
+        $baseUrl = $this->scopeConfig->getValue('web/secure/base_url');
+        return $baseUrl . '/braintree/kount/ens';
     }
 }
