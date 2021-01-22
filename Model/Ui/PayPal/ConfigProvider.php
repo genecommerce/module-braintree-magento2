d79bf084d960ef0a37757f037823f1cf9e77836a
Dima <ladonenko@gmail.com>
2021-01-22 16:12:46 +0200

    Paypal not loading on the checkout for Norway

--- Model/Ui/PayPal/ConfigProvider.php
+++ Model/Ui/PayPal/ConfigProvider.php
@@ -68,6 +68,10 @@ class ConfigProvider implements ConfigProviderInterface
      */
     public function getConfig(): array
     {
+        $locale = $this->resolver->getLocale();
+        if (in_array($locale, ['nb_NO', 'nn_NO'])) {
+            $locale = 'no_NO';
+        }
         return [
             'payment' => [
                 self::PAYPAL_CODE => [
@@ -75,7 +79,7 @@ class ConfigProvider implements ConfigProviderInterface
                     'title' => $this->config->getTitle(),
                     'isAllowShippingAddressOverride' => $this->config->isAllowToEditShippingAddress(),
                     'merchantName' => $this->config->getMerchantName(),
-                    'locale' => $this->resolver->getLocale(),
+                    'locale' => $locale,
                     'paymentAcceptanceMarkSrc' =>
                         'https://www.paypalobjects.com/webstatic/en_US/i/buttons/pp-acceptance-medium.png',
                     'vaultCode' => self::PAYPAL_VAULT_CODE,
@@ -93,7 +97,7 @@ class ConfigProvider implements ConfigProviderInterface
                     'title' => __('PayPal Credit'),
                     'isAllowShippingAddressOverride' => $this->config->isAllowToEditShippingAddress(),
                     'merchantName' => $this->config->getMerchantName(),
-                    'locale' => $this->resolver->getLocale(),
+                    'locale' => $locale,
                     'paymentAcceptanceMarkSrc' =>
                         'https://www.paypalobjects.com/webstatic/en_US/i/buttons/ppc-acceptance-medium.png',
                     'paymentIcon' => $this->config->getPayPalIcon(),
@@ -110,7 +114,7 @@ class ConfigProvider implements ConfigProviderInterface
                     'title' => __('PayPal PayLater'),
                     'isAllowShippingAddressOverride' => $this->config->isAllowToEditShippingAddress(),
                     'merchantName' => $this->config->getMerchantName(),
-                    'locale' => $this->resolver->getLocale(),
+                    'locale' => $locale,
                     'paymentAcceptanceMarkSrc' =>
                         'https://www.paypalobjects.com/webstatic/en_US/i/buttons/ppc-acceptance-medium.png',
                     'paymentIcon' => $this->config->getPayPalIcon(),
