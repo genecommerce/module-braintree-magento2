aaa4919faa5f0798a865d7e0697e5ef5383708ed
kartikmaniyar <kartik.maniyar@gmail.com>
2022-07-29 16:10:20 +0530

    Render braintree payment components on checkout only if enabled.

--- view/frontend/web/js/applepay/implementations/core-checkout/method-applepay.js
+++ view/frontend/web/js/applepay/implementations/core-checkout/method-applepay.js
@@ -1,14 +1,26 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 define(
-    ['uiComponent', 'Magento_Checkout/js/model/payment/renderer-list'],
-    function (Component, rendererList) {
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
         'use strict';
 
-        rendererList.push(
-            {
+        let config = window.checkoutConfig.payment;
+
+        if (config['braintree_applepay'].clientToken) {
+            rendererList.push({
                 type: 'braintree_applepay',
                 component: 'Magento_Braintree/js/applepay/implementations/core-checkout/method-renderer/applepay'
-            }
-        );
+            });
+        }
 
         return Component.extend({});
     }
--- view/frontend/web/js/googlepay/implementations/core-checkout/method-googlepay.js
+++ view/frontend/web/js/googlepay/implementations/core-checkout/method-googlepay.js
@@ -1,14 +1,26 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 define(
-    ['uiComponent', 'Magento_Checkout/js/model/payment/renderer-list'],
-    function (Component, rendererList) {
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
         'use strict';
 
-        rendererList.push(
-            {
+        let config = window.checkoutConfig.payment;
+
+        if (config['braintree_googlepay'].clientToken) {
+            rendererList.push({
                 type: 'braintree_googlepay',
                 component: 'Magento_Braintree/js/googlepay/implementations/core-checkout/method-renderer/googlepay'
-            }
-        );
+            });
+        }
 
         return Component.extend({});
     }
